16b47deded78e23d8a733b7f7e81c7ed014672ca
ALEXX <114208686+ALEXXHUMILDE@users.noreply.github.com>
2024-08-08 11:33:57 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
+  return (
+    
+    <div className="currency-converter">
+      <h2 className="converter-title">Convertidor de moneda</h2>
+      <ConverterForm />
+    </div>
+  )
+}
+
+export default App;
